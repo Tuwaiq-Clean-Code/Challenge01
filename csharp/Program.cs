1c49f52b5c85483c7760e25236457c4edd92f06c
amalFreeh1 <amal.almotairi@tuwaiq.edu.sa>
2021-06-29 21:20:47 +0300

    upload files

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,55 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
 namespace csharp
 {
-    public class Program
+    public class GildedRose
     {
-        public static void Main(string[] args)
+        bool IsValidName(string name)
         {
-            Console.WriteLine("OMGHAI!");
-
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
-
-            var app = new GildedRose(Items);
-
-
-            for (var i = 0; i < 31; i++)
+            return (!name.Equals("Aged Brie") &&
+                    !name.Equals("Backstage passes to a TAFKAL80ETC concert") &&
+                    !name.Equals("Sulfuras, Hand of Ragnaros"));
+        }
+        bool IsMoreThanZero(int num)
+        {
+            return num > 0;
+        }
+        bool isLessThanFifty(int num)
+        {
+            return num < 50;
+        }
+        IList<Item> Items;
+        GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+        void UpdateQuality()
+        {
+            foreach (var item in Items)
             {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                if (IsValidName(item.Name) && IsMoreThanZero(item.Quality))
                 {
-                    System.Console.WriteLine(Items[j]);
+                    item.Quality -= 1;
                 }
-                Console.WriteLine("");
-                app.UpdateQuality();
+                else
+                {
+                    if (isLessThanFifty(item.Quality))
+                    {
+                        item.Quality = item.Quality + 1;
+                        if (isLessThanFifty(item.Quality) && item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.SellIn < 11)
+                                item.Quality += item.Quality;
+                        }
+                    }
+                }
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    item.SellIn =- 1;
+                if (item.SellIn < 0)
+                {
+                    if (item.Name != "Aged Brie")
+                    {
+                        if (IsValidName(item.Name) && IsMoreThanZero(item.Quality))
+                            item.Quality -= 1;
+                        else
+                            item.Quality -= item.Quality;
+                    }
+                    else
+                    {
+                        if (isLessThanFifty(item.Quality))
+                            item.Quality += 1;
+                    }
+                }
             }
         }
     }
