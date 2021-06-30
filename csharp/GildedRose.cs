696877c5a824642806475cabc6ce42a2a3532df7
Abdulmajeed Almaymuni <Abdulmajeed.Almaymuni@tuwaiq.edu.sa>
2021-06-30 22:22:04 +0300

    Add Solution

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -12,78 +12,79 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if(isValidItemName(item.Name))
                 {
-                    if (Items[i].Quality > 0)
+                    if (hasPositiveQuality(item.Quality))
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
+                        item.Quality -= 1;
                     }
+                    
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (hasQualityUnderFifty(item.Quality))
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        item.Quality += 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if(item.SellIn < 11 && hasQualityUnderFifty(item.Quality))
                         {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                            item.Quality += 1;
+                        }
+                        if (item.SellIn < 6 && hasQualityUnderFifty(item.Quality))
+                        {
+                            item.Quality += 1;
                         }
                     }
                 }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    item.SellIn -=  1;
                 }
 
-                if (Items[i].SellIn < 0)
+                if(item.SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if(item.Name != "Aged Brie")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if(item.Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Items[i].Quality > 0)
+                            if(hasPositiveQuality(item.Quality) && item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                item.Quality -= 1;
                             }
-                        }
-                        else
+                        } else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            item.Quality -= item.Quality;
                         }
                     }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                } else if (hasQualityUnderFifty(item.Quality))
+                {
+                    item.Quality += 1;
                 }
             }
         }
+
+        public bool isValidItemName(string name)
+        {
+            if(name != "Aged Brie" 
+                && name != "Backstage passes to a TAFKAL80ETC concert" 
+                && name != "Sulfuras, Hand of Ragnaros")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool hasPositiveQuality(int quality)
+        {
+            return quality > 0;
+        }
+
+        public bool hasQualityUnderFifty(int quality)
+        {
+            return quality < 50;
+        }
+
     }
 }
