ad530457e967474cd991036ba000f795c516b99b
Raneen-z <raneen.alzafarani@tuwaiq.edu.sa>
2021-06-30 11:31:51 +0300

    update readme

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -10,11 +10,15 @@ namespace csharp
             this.Items = Items;
         }
 
+        public static bool CheckName(name)
+        {
+            return (name != "Aged Brie" && name != "Backstage passes to a TAFKAL80ETC concert")
+        }
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (CheckName(Items[i].Name))
                 {
                     if (Items[i].Quality > 0)
                     {
@@ -34,18 +38,7 @@ namespace csharp
                         {
                             if (Items[i].SellIn < 11)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                Items[i].Quality = Items[i].Quality + 1;
                             }
                         }
                     }
